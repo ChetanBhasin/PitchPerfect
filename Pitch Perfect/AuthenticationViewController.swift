3611ddea256ca92a3f918db3bb97982d4a30860c
Chetan Bhasin <connect@chetanbhasin.com>
2018-09-02 10:22:41 +0200

    Preparing to add an authentication view controller

---
+++ Pitch Perfect/AuthenticationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AuthenticationViewController.swift
+//  Pitch Perfect
+//
+//  Created by Chetan Bhasin on 02.09.18.
+//  Copyright © 2018 Chetan Bhasin. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+    
+    var authenticated: Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
